e3791b03b6c14000aa887d426a4e95aca45e971f
Sgeo <sgeoster@gmail.com>
2018-06-02 16:28:19 -0400

    Some error recording, and hex edit EasyHook32 to allow a different name for the injection point.

--- aw_sbs/src/lib.rs
+++ aw_sbs/src/lib.rs
@@ -1,24 +1,32 @@
+#![crate_type="cdylib"]
+
 extern crate easyhook;
 extern crate libloading as lib;
 
 #[macro_use]
 extern crate lazy_static;
 
+use std::io::Write;
 use std::os::raw::{c_ulong, c_void};
 use easyhook::lh_install_hook;
+use easyhook::error_string;
 
 lazy_static! {
     static ref GL: lib::Library = lib::Library::new("OPENGL32").unwrap();
     static ref glViewport: lib::Symbol<'static, extern "system" fn(i32, i32, u32, u32)> = unsafe { GL.get(b"glViewport\0") }.unwrap();
 }
 
-#[no_mangle]
+#[export_name="_NativeInjectionEntryPoint_4"]
 pub extern "stdcall" fn NativeInjectionEntryPoint(_remote_info: *mut c_void) {
     unsafe {
         use std::fs::File;
         File::create("about_to_install_hook.txt").unwrap();
         lh_install_hook(**glViewport as *mut _, glViewportHook as *mut _);
+        let error = error_string();
         File::create("installed_hook.txt").unwrap();
+        let mut errors = File::create("hook_errors.txt").unwrap();
+        writeln!(&mut errors, "Error: {:?}", error);
+        drop(errors);
     }
 }
 
