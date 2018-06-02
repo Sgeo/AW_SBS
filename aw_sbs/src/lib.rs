5d39440f20bf6c323a261e84f9187d49dffef49d
Sgeo <sgeoster@gmail.com>
2018-06-02 18:45:35 -0400

    Frame translation.

--- aw_sbs/src/lib.rs
+++ aw_sbs/src/lib.rs
@@ -8,12 +8,16 @@ extern crate lazy_static;
 
 use std::io::Write;
 use std::os::raw::{c_ulong, c_void};
+use std::sync::atomic::{AtomicUsize, Ordering};
 use easyhook::{lh_install_hook};
 use easyhook::error_string;
 
 lazy_static! {
     static ref GL: lib::Library = lib::Library::new("OPENGL32").unwrap();
+    static ref RW: lib::Library = lib::Library::new("rw_opengl").unwrap();
     static ref glViewport: lib::Symbol<'static, extern "system" fn(i32, i32, u32, u32)> = unsafe { GL.get(b"glViewport\0") }.unwrap();
+    static ref rw_camera_begin_update: lib::Symbol<'static, extern "C" fn(*mut c_void) -> *mut c_void> = unsafe { RW.get(b"rw_camera_begin_update\0") }.unwrap();
+    static ref rw_frame_translate: lib::Symbol<'static, extern "C" fn(*mut c_void, *mut f32, u32) -> *mut c_void> = unsafe { RW.get(b"rw_frame_translate\0") }.unwrap();
 }
 
 #[export_name="_NativeInjectionEntryPoint_4"] // EasyHook32.dll has been hex edited to look for this
@@ -30,6 +34,32 @@ pub extern "stdcall" fn NativeInjectionEntryPoint(_remote_info: *mut c_void) {
     }
 }
 
+static counter: AtomicUsize = AtomicUsize::new(0);
+
+
 pub extern "system" fn glViewportHook(x: i32, y: i32, width: u32, height: u32) {
-    glViewport(x, y, width/2, height);
+    let current = counter.load(Ordering::SeqCst);
+    counter.store(current + 1, Ordering::SeqCst);
+    if current&2 == 0 {
+        glViewport(x, y, width/2, height);
+    } else {
+        glViewport((width/2) as i32, y, width/2, height);
+    }
+}
+
+fn camera_get_frame(camera: *mut c_void) -> *mut c_void {
+    unsafe {
+        let camera_as_ptrs = camera as *mut usize;
+        let ptr_to_frame = camera_as_ptrs.offset(1);
+        *ptr_to_frame as *mut c_void
+    }
+}
+
+pub extern "C" fn rw_camera_begin_update_hook(camera: *mut c_void) -> *mut c_void {
+    let current = counter.load(Ordering::SeqCst);
+    if current&2 != 0 {
+        let frame = camera_get_frame(camera);
+        rw_frame_translate(frame, (&mut [0.006, 0.0, 0.0]).as_mut_ptr(), 1);
+    }
+    camera
 }
